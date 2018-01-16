ecddd74969ba076c9cd0dee6d8c7e91bf08e3dcd
PDSamson <pdsamson72@gmail.com>
2018-01-16 14:57:57 -0500

    Add user id column to boat resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116191533) do
+ActiveRecord::Schema.define(version: 20180116193512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20180116191533) do
     t.boolean "stored"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -60,6 +62,7 @@ ActiveRecord::Schema.define(version: 20180116191533) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "boats", "users"
   add_foreign_key "examples", "users"
   add_foreign_key "projects", "boats"
 end
