53a8b4686c97a1f8edb82d62a29657f2446b1327
PDSamson <pdsamson72@gmail.com>
2018-01-16 14:58:50 -0500

    Add relevant models to user, boat and project serializers
    -Add belongs_to :boat to project serializer
    -Add has_many :projects and belongs_to :user to boat serializer
    -Add has_many :boats to user serializer

--- app/serializers/boat_serializer.rb
+++ app/serializers/boat_serializer.rb
@@ -1,3 +1,5 @@
 class BoatSerializer < ActiveModel::Serializer
   attributes :id, :name, :ownerName, :ownerEmail, :ownerPhone, :length, :boatType, :make, :year, :stored
+  has_many :projects
+  belongs_to :user
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,4 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :task_title, :description, :contractor, :bid, :completion_date, :payment
+  belongs_to :boat
 end
